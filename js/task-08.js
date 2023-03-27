02d51873f6819b8e62d730e51fe0798a4ccd0856
Kostyantyn <118232131+folkUA@users.noreply.github.com>
2023-03-27 16:24:18 -0400

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -3,7 +3,7 @@ const userForm = document.querySelector(".login-form");
 userForm.addEventListener("submit", onSubmit);
 function onSubmit(evt) {
   evt.preventDefault();
-  const { email, password } = this.elements;
+  const { email, password } = evt.currentTarget.elements;
   if (!password.value || !email.value) {
     alert("Всі поля обов'язкові для заповнення");
   } else {
